30294cecb3771d942c7bce1feeec664547396c5a
Ebert Guerreiro <ebertg.est@gmail.com>
2024-07-09 20:37:32 -0300

    a

---
+++ curso/aula014/ex016/script.js
@@ -0,0 +1,33 @@
+function contar() {
+    let inicio = document.getElementById("inicio");
+    let fim = document.getElementById("fim");
+    let passo = document.getElementById("passo");
+    let res = document.querySelector("div#res");
+    let i = Number(inicio.value);
+    let f = Number(fim.value);
+    let p = Number(passo.value);
+    if (inicio.value.length == 0) {
+        i = 1;
+    }
+    if (fim.value.length == 0) {
+        alert('Erro! O valor de "FIM" Não foi definido.');
+        return;
+    }
+    if (passo.value.length == 0) {
+        p = 1;
+    }
+
+    if (i < f) {
+        let contagem = i;
+        for (i; contagem <= f; contagem += p) {
+            res.innerHTML += `${contagem} \u{1F449} `;
+        }
+    } else {
+        let contagem = i;
+        for (i; contagem >= f; contagem -= p) {
+            res.innerHTML(`${contagem} \u{1F449}`);
+        }
+    }
+    res.innerHTML += `\u{1F3C1}`;
+}
+//NÃO ESTÁ FUNCIONANDO A CONTAGEM DE TRÁS PRA FRENTE!!!!
